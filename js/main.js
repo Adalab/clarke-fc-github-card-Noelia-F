06f0e8637c08b7423ccb514c2d5e4839d0afe17c
Noelia-F <noelia.fronton.g@gmail.com>
2018-04-22 13:29:58 +0200

    add elements depends to user

--- js/main.js
+++ js/main.js
@@ -1,12 +1,12 @@
 'use strict';
 const initial = function(){
-  let adalabUsers = [];
-  let adalabInfo = {};
-  const selectUser = document.querySelectorById('select__user');
+  let adalabMembers = [];
+  let memberInfo = {};
+  const selectUser = document.querySelector('.select__user');
   const userBox = document.querySelector('.user__box');
   const memberSince = document.querySelector('.user__since');
 
-  fetch('https://api.github.com/org/adalab/public_members?per_page=68')
+  fetch('https://api.github.com/orgs/adalab/public_members?per_page=68')
     .then(function(response){
         return response.json();
     })
@@ -16,7 +16,7 @@ const initial = function(){
       for(let i=0; i<adalabMembers.length; i++){
         let optionUser = document.createElement('option');
         optionUser.value = adalabMembers[i].login;
-        optionUser.innerHtml = adalabUsers[i].login;
+        optionUser.innerHTML = adalabMembers[i].login;
         selectUser.appendChild(optionUser);
       }
     });
@@ -36,5 +36,41 @@ const initial = function(){
       location.reload();
     }
   });
-  
+  function renderUserInfo(memberInfo){
+    let imageLocation = '<img class="location" src="images/location.svg" alt="location">';
+    if(memberInfo.location === null){
+      memberInfo.location = '';
+    }
+    if(memberInfo.name === null){
+      memberInfo.name = '';
+    }
+
+    userBox.innerHTML = `
+    <img class="member-avatar" src=https//avatars1.githubusercontent.com/u/${memberInfo.id} alt="memeber avatar">
+    <div class="member-info">
+      <h1 class="member__user-name">@${memberInfo.login}</h1>
+      <h2 class="member__name">${memberInfo.name}</h2>
+      <div class="member__location-box">
+        ${imageLocation}
+        <p class="member__location">${memberInfo.location}</p>
+      </div>
+    </div>
+    <div class="member-social">
+      <div>
+        <h3 class="member-social__title">${memberInfo.public_repos}</h3>
+        <p class="member-social__info">Repos</p>
+      </div>
+      <div>
+        <h3 class="member-social__title">${memberInfo.followers}</h3>
+        <p class="member-social__info">Followers</p>
+      </div>
+      <div>
+        <h3 class="member-social__title">${memberInfo.following}</h3>
+        <p class="member-social__info">Following</p>
+      </div>
+    </div>`;
+    memberSince.innerHTML = `Miembro desde ${new Date(memberInfo.created_at).getFullYear()}`;
+  }
 }
+
+initial();
